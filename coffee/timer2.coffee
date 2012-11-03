68094b8e554fe80050b198a294bf877085a6e341
Lukas Marek <lukas.marek@gmail.com>
2012-11-03 17:08:33 +0100

    Clean up and reformat files

--- coffee/timer2.coffee
+++ coffee/timer2.coffee
@@ -6,187 +6,189 @@ total_time = 0
 actual_time = 0
 timer_running = false
 
-window.getTea = (name) -> 
-	for tea in teas
-		if (tea.name == name)
-			return tea
+window.getTea = (name) ->
+  for tea in teas
+    if (tea.name == name)
+      return tea
 
 window.storeTea = (tea) ->
-	localStorage[CHOSEN_TEA] = tea.name
+  localStorage[CHOSEN_TEA] = tea.name
 
 window.createRadios = () ->
-	stored_tea = localStorage[CHOSEN_TEA]
-	for tea in teas
-		$("<label class='radio'><input type='radio' name='time' value='#{tea.name}'>#{tea.title}</label>").appendTo($('#radios'))
-		
-	if stored_tea
-		for radio in $('input:radio[name=time]')
-			if radio.value == stored_tea
-				radio.checked = true
-	else 
-		$('input:radio[name=time]')[0].checked = true
+  stored_tea = localStorage[CHOSEN_TEA]
+  for tea in teas
+    $("<label class='radio'><input type='radio' name='time' value='#{tea.name}'>#{tea.title}</label>").appendTo($('#radios'))
 
-window.startTimer = (seconds, btn) ->   
-	window.btn = btn
-	permission = window.webkitNotifications.checkPermission() 
-	time = 1000 * seconds
-	total_time = time
-	actual_time = time
-	console.log("Setting timeout for #{time}")  
-	timer_running = true
-	setTimeout("onTick()", 1000)
-	if (permission != 0) 
-		window.webkitNotifications.requestPermission(startTimer(minutes))			
-	$('#countdownModal').modal()
-	$('#countdownBar').css("width", "100%")
-	$('#countdownTime').html(formatMillis(actual_time))
+  if stored_tea
+    for radio in $('input:radio[name=time]')
+      if radio.value == stored_tea
+        radio.checked = true
+  else
+    $('input:radio[name=time]')[0].checked = true
 
-
-
-window.onTick = () -> 
-	actual_time = actual_time - 1000  
-	if (timer_running)
-		percentage = actual_time / (total_time / 100)
-		$('#countdownBar').css("width", percentage + "%")
-		$('#countdownTime').html(formatMillis(actual_time))
-
-		if actual_time >= 0
-    		setTimeout("onTick()", 1000)
-		else
-			setTimeout("onTimeout()", 0)
+window.startTimer = (seconds, btn) ->
+  window.btn = btn
+  permission = window.webkitNotifications.checkPermission()
+  time = 1000 * seconds
+  total_time = time
+  actual_time = time
+  console.log("Setting timeout for #{time}")
+  timer_running = true
+  setTimeout("onTick()", 1000)
+  if (permission != 0)
+    window.webkitNotifications.requestPermission(startTimer(minutes))
+  $('#countdownModal').modal()
+  $('#countdownBar').css("width", "100%")
+  $('#countdownTime').html(formatMillis(actual_time))
 
 
-window.formatMillis = (millis) -> 
-	secs = millis/1000
-	minutes = Math.floor(secs/60)
-	secs_remainder = secs - (minutes * 60) 
-	if (secs_remainder) < 10
-		secs_remainder = "0" + secs_remainder
-	return minutes + ":" + secs_remainder
+window.onTick = () ->
+  actual_time = actual_time - 1000
+  if (timer_running)
+    percentage = actual_time / (total_time / 100)
+    $('#countdownBar').css("width", percentage + "%")
+    $('#countdownTime').html(formatMillis(actual_time))
+
+    if actual_time >= 0
+      setTimeout("onTick()", 1000)
+    else
+      setTimeout("onTimeout()", 0)
+
+
+window.formatMillis = (millis) ->
+  secs = millis / 1000
+  minutes = Math.floor(secs / 60)
+  secs_remainder = secs - (minutes * 60)
+  if (secs_remainder) < 10
+    secs_remainder = "0" + secs_remainder
+  return minutes + ":" + secs_remainder
 
 
 window.onTimeout = () ->
-	permission = window.webkitNotifications.checkPermission()
-	console.log("Permission: #{permission}")
-	if (permission == 0) 
-		displayNotification()
-		$('#countdownModal').modal("hide")		
-	else 
-		console.log("Průšvih - nemám permission!")	
-	enable()
+  permission = window.webkitNotifications.checkPermission()
+  console.log("Permission: #{permission}")
+  if (permission == 0)
+    displayNotification()
+    $('#countdownModal').modal("hide")
+  else
+    console.log("Průšvih - nemám permission!")
+  enable()
 
 
-window.onSliderChange = (evt, ui) -> 
-	time = ui.value * 1000
-	$('#teaTime').html(formatMillis(time))
+window.onSliderChange = (evt, ui) ->
+  time = ui.value * 1000
+  $('#teaTime').html(formatMillis(time))
 
 
-window.displayNotification = () -> 
-	permission = window.webkitNotifications.checkPermission()
-	console.log("Permission: #{permission}")
-	window.popup = window.webkitNotifications.createNotification("img/icon.png", 
-		window.notificationTemplate.title, 
-		window.notificationTemplate.body)
+window.displayNotification = () ->
+  permission = window.webkitNotifications.checkPermission()
+  console.log("Permission: #{permission}")
+  window.popup = window.webkitNotifications.createNotification("img/icon.png",
+    window.notificationTemplate.title,
+    window.notificationTemplate.body)
 
-	ding('snd/alarm.mp3', window.popup)		
-	popup.show()
-	setTimeout("popup.cancel()", CANCEL_TIMEOUT)
+  ding('snd/alarm.mp3', window.popup)
+  popup.show()
+  setTimeout("popup.cancel()", CANCEL_TIMEOUT)
 
-window.disableGroup = (group) -> 
-	for item in group	
-		item.disabled = true
+window.disableGroup = (group) ->
+  for item in group
+    item.disabled = true
 
 window.enableGroup = (group) ->
-	for item in group	
-		item.disabled = false
+  for item in group
+    item.disabled = false
 
 window.updateInfoPanel = (tea) ->
-	$('#teaName').button().html(tea.title)
-	$('#teaTime').button().html(window.formatMillis(tea.time * 1000))
-	$('#teaTemp').button().html(tea.temp)
+  $('#teaName').button().html(tea.title)
+  $('#teaTime').button().html(window.formatMillis(tea.time * 1000))
+  $('#teaTemp').button().html(tea.temp)
 
 
-window.ding = (mp3, popup) -> 
-	snd = new Audio(mp3)
-	if SOUND 
-		snd.play()
-		###
-		stop playing alert sound when notification popup is closed 
-		(synchronized with notification popup through CANCEL_TIMEOUT
-		constant which is same in both cases)		 
-		###
-		pauseAudio = -> snd.pause()
-		popup.onclose = pauseAudio
-		popup.onclick = pauseAudio
-		setTimeout(pauseAudio, CANCEL_TIMEOUT) 
-		
+window.ding = (mp3, popup) ->
+  snd = new Audio(mp3)
+  if SOUND
+    snd.play()
+    ###
+     stop playing alert sound when notification popup is closed
+     (synchronized with notification popup through CANCEL_TIMEOUT
+     constant which is same in both cases)
+     ###
+    pauseAudio = -> snd.pause()
+    popup.onclose = pauseAudio
+    popup.onclick = pauseAudio
+    setTimeout(pauseAudio, CANCEL_TIMEOUT)
+
 window.enable = () ->
-	$('#btn-run').button('reset')
-	enableGroup($('input:radio[name=time]'));
-	$("#slider").slider("enable");
-	time = $("#slider").slider("option", "value")
-	$('#teaTime').html(formatMillis(time * 1000))
-	$('#btn-reset').attr("disabled", "true")
-	timer_running = false
-	$('#countdownModal').modal("hide")
+  $('#btn-run').button('reset')
+  enableGroup($('input:radio[name=time]'))
+  ;
+  $("#slider").slider("enable")
+  ;
+  time = $("#slider").slider("option", "value")
+  $('#teaTime').html(formatMillis(time * 1000))
+  $('#btn-reset').attr("disabled", "true")
+  timer_running = false
+  $('#countdownModal').modal("hide")
 
 
 #Initialization code
 $(document).ready ->
-	#popovers
-	$("a[rel=popover]").popover(offset: 10).click = (e) ->
-		e.preventDefault()
-	#radios
-	createRadios()
-	
-	#slider
-	$("#slider").slider({min:1, max: 659})
-	
-	#run button function
-	$('#btn-run').click -> 
-		permission = window.webkitNotifications.checkPermission()
-		if (permission != 0) 
-			window.webkitNotifications.requestPermission()
-		time =  $("#slider").slider( "option", "value" )
-		localStorage[CUSTOM_TIMER] = time
-		console.log("Setting timer for: " + time)
-		$('#btn-run').button('loading');
-		startTimer(time, $('#teaTime'))
-		disableGroup($('input:radio[name=time]'))
-		$("#slider").slider("disable")
-		$('#btn-reset').removeAttr("disabled")
-		_gaq.push(['_trackEvent', 'start-time', time.toString()])
-		_gaq.push(['_trackEvent', 'start-tea', localStorage[CHOSEN_TEA]])		
-		$('#countdownLabel').text(getTea(localStorage[CHOSEN_TEA]).title)
+  #popovers
+  $("a[rel=popover]").popover(offset: 10).click = (e) ->
+    e.preventDefault()
+  #radios
+  createRadios()
 
-		
-	#reset button function
-	$('#btn-reset').click ->
-		window.enable()					
-	
-	#radio button functions
-	$('input:radio[name=time]').click ->
-		name = $('input:radio[name=time]:checked').val();
-		tea = getTea(name)
-		storeTea(tea)
-		updateInfoPanel(tea)
-		$('#slider').slider("option", "value", tea.time);
+  #slider
+  $("#slider").slider({min: 1, max: 659})
+
+  #run button function
+  $('#btn-run').click ->
+    permission = window.webkitNotifications.checkPermission()
+    if (permission != 0)
+      window.webkitNotifications.requestPermission()
+    time =  $("#slider").slider("option", "value")
+    localStorage[CUSTOM_TIMER] = time
+    console.log("Setting timer for: " + time)
+    $('#btn-run').button('loading')
+    startTimer(time, $('#teaTime'))
+    disableGroup($('input:radio[name=time]'))
+    $("#slider").slider("disable")
+    $('#btn-reset').removeAttr("disabled")
+    _gaq.push(['_trackEvent', 'start-time', time.toString()])
+    _gaq.push(['_trackEvent', 'start-tea', localStorage[CHOSEN_TEA]])
+    $('#countdownLabel').text(getTea(localStorage[CHOSEN_TEA]).title)
+
+  #reset button function
+  $('#btn-reset').click ->
+    window.enable()
+
+  #radio button functions
+  $('input:radio[name=time]').click ->
+    name = $('input:radio[name=time]:checked').val()
+    ;
+    tea = getTea(name)
+    storeTea(tea)
+    updateInfoPanel(tea)
+    $('#slider').slider("option", "value", tea.time)
+    ;
 
 
-	$( "#slider" ).slider(slide: (event, ui) ->
-		onSliderChange(event, ui))
-	$( "#slider" ).slider(change: (event, ui) ->
-		onSliderChange(event, ui))
-	
-	#check webkit notification
-	if (!window.webkitNotifications)
-		$(".alert").show()
-		$('#btn-run').toggleClass('disabled')
-		
-	$('input:radio[name=time]:checked').click()
-	if localStorage[CUSTOM_TIMER] 
-		$("#slider").slider("option", "value", localStorage[CUSTOM_TIMER])
+  $("#slider").slider(slide: (event, ui) ->
+    onSliderChange(event, ui))
+  $("#slider").slider(change: (event, ui) ->
+    onSliderChange(event, ui))
 
-	$('#countdownModal').on(hidden: () ->
-		window.enable()
-	)
+  #check webkit notification
+  if (!window.webkitNotifications)
+    $(".alert").show()
+    $('#btn-run').toggleClass('disabled')
+
+  $('input:radio[name=time]:checked').click()
+  if localStorage[CUSTOM_TIMER]
+    $("#slider").slider("option", "value", localStorage[CUSTOM_TIMER])
+
+  $('#countdownModal').on(hidden: () ->
+    window.enable()
+  )
--- index.html.cs
+++ index.html.cs
@@ -9,156 +9,178 @@
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
     <!-- Le styles -->
     <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="css/bootstrap-responsive.min.css">
-    <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
-	<link type="text/css" href="css/override.css" rel="stylesheet"/>
+    <link type="text/css" href="css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="stylesheet"/>
+    <link type="text/css" href="css/override.css" rel="stylesheet"/>
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="img/favicon.ico">
     <link rel="apple-touch-icon" href="img/favicon.ico">
-		
-<!-- Place this render call where appropriate -->
-	<script type="text/javascript">
-	window.___gcfg = {lang: 'cs'};
-	
-  	(function() {
-    		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    		po.src = 'https://apis.google.com/js/plusone.js';
-    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
-	</script>
+
+    <!-- Place this render call where appropriate -->
+    <script type="text/javascript">
+        window.___gcfg = {lang:'cs'};
+
+        (function () {
+            var po = document.createElement('script');
+            po.type = 'text/javascript';
+            po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(po, s);
+        })();
+    </script>
 
 
-  </head>
+</head>
 
-  <body>
-    <div class="container">                    
-        <div class="page-header"> <h1>Tea-clock <small>Ať se louhuje...</small></h1> </div>
-		<div class="row">
-			<div class="span10">
-				<div class="alert alert-block alert-error" hidden="true">
-					<a class="close" data-dismiss="alert">×</a>
-					<h4 class="alert-heading">Je vyžadován Chrome!</h4>
-				  	Tato aplikace potřebuje ke svému běhu <strong>desktopové notifikace</strong>, které aktuálně fungují pouze v <strong>Google Chrome.</strong>
-				</div>
-			</div>
-		</div>
-        <div class="row">
-          <div class="span7">
-			<div class="well">
-	            <form id="form1" class="form-horizontal">
-					<fieldset>
-						<legend>Jak pijete svůj čaj?</legend>
-							<div class="control-group">							
-								<label class="control-label">Druh čaje:</label>								
-	                			<div class="controls" id="radios">          
-	                			</div>
-	                		</div>
-		              		<div class="control-group">
-								<label class="control-label" for="slider">Úprava času:</label>
-								<div class="controls">
-									<div class="span4" id="slider"></div>
-								</div>
-							</div>						
-	              	</fieldset>	              	
-	              		              						
-	              <div class="form-actions">
-	                <button id="btn-run" data-loading-text="Louhuju" class="btn btn-danger btn-large" type="button">
-						<i class="icon-time icon-white"></i>
-						Louhuj
-					</button>&nbsp;
-	              </div>
-	            </form>
-	            
-			</div>			
-          </div>
-          
-		  <div class="span3" style="text-align:center">
-			<div class="well">
-				<div class="clearfix">
-					<img src="img/icon_pruhledna.png"><br/><br/>
-				</div>
-				<div class="clearfix">
-					<a href="#" class="btn btn-default btn-large" style="width:120px" id="teaName"></a><br/><br/>
-				</div>
-				<div class="clearfix">
-					<a href="#" class="btn btn-default btn-large" style="width:120px" rel="popover" id="teaTime" data-content="Jak dlouho se bude čaj louhovat." data-original-title="Čas">1:00</a><br/><br/>
-				</div>
-				<div class="clearfix">
-					<a href="#" class="btn btn-default btn-large" style="width:120px" rel="popover" id="teaTemp" data-content="Při této teplotě by se měl vybraný čaj louhovat." data-original-title="Teplota">60C</a>
-				</div>
-				
-			</div>
-		  </div>
-		</div>
-		<div class="row">
-			<div class="span7">				
-	            <p><g:plusone annotation="inline" href="https://chrome.google.com/webstore/detail/hmldmlgafdbnfhhicheojakimpmocggp"></g:plusone></p>
-	          	
-			</div>
-		</div>	
-        
+<body>
+<div class="container">
+    <div class="page-header">
+        <h1>Tea-clock
+            <small>Ať se louhuje...</small>
+        </h1>
+    </div>
+    <div class="row">
+        <div class="span10">
+            <div class="alert alert-block alert-error" hidden="hidden">
+                <a class="close" data-dismiss="alert">×</a>
+                <h4 class="alert-heading">Je vyžadován Chrome!</h4>
+                Tato aplikace potřebuje ke svému běhu <strong>desktopové notifikace</strong>, které aktuálně fungují pouze v
+                <strong>Google Chrome.</strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="span7">
+            <div class="well">
+                <form id="form1" class="form-horizontal">
+                    <fieldset>
+                        <legend>Jak pijete svůj čaj?</legend>
+                        <div class="control-group">
+                            <label class="control-label">Druh čaje:</label>
 
-      <footer>
-<!--           <p>&copy; lukas.marek (at) gmail.com, 2011</p> -->
-	  	<script type="text/javascript">
+                            <div class="controls" id="radios">
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label" for="slider">Úprava času:</label>
 
-			  var _gaq = _gaq || [];
-			  _gaq.push(['_setAccount', 'UA-339099-9']);
-			  _gaq.push(['_trackPageview']);
+                            <div class="controls">
+                                <div class="span4" id="slider"></div>
+                            </div>
+                        </div>
+                    </fieldset>
 
-			  (function() {
-    			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			  })();
+                    <div class="form-actions">
+                        <button id="btn-run" data-loading-text="Louhuju" class="btn btn-danger btn-large" type="button">
+                            <i class="icon-time icon-white"></i>
+                            Louhuj
+                        </button>
+                        &nbsp;
+                    </div>
+                </form>
 
-		</script>
-      </footer>
+            </div>
+        </div>
 
-<a href="https://github.com/krtek/Tea-clock">
-	<img style="position: absolute; z-index: 5; top: 0; right: 0; border: 0;"
-		 src="http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
-		 alt="Fork me on GitHub" />
-</a>
+        <div class="span3" style="text-align:center">
+            <div class="well">
+                <div class="clearfix">
+                    <img src="img/icon_pruhledna.png"><br/><br/>
+                </div>
+                <div class="clearfix">
+                    <a href="#" class="btn btn-default btn-large" style="width:120px" id="teaName"></a><br/><br/>
+                </div>
+                <div class="clearfix">
+                    <a href="#" class="btn btn-default btn-large" style="width:120px" rel="popover" id="teaTime"
+                       data-content="Jak dlouho se bude čaj louhovat." data-original-title="Čas">1:00</a><br/><br/>
+                </div>
+                <div class="clearfix">
+                    <a href="#" class="btn btn-default btn-large" style="width:120px" rel="popover" id="teaTemp"
+                       data-content="Při této teplotě by se měl vybraný čaj louhovat." data-original-title="Teplota">60C</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="span7">
+            <p>
+                <g:plusone annotation="inline"
+                           href="https://chrome.google.com/webstore/detail/hmldmlgafdbnfhhicheojakimpmocggp"></g:plusone>
+            </p>
+
+        </div>
+    </div>
 
 
-    </div> <!-- /container -->
+    <footer>
+        <!--           <p>&copy; lukas.marek (at) gmail.com, 2011</p> -->
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-339099-9']);
+            _gaq.push(['_trackPageview']);
+
+            (function () {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
+    </footer>
+
+    <a href="https://github.com/krtek/Tea-clock">
+        <img style="position: absolute; z-index: 5; top: 0; right: 0; border: 0;"
+             src="http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+             alt="Fork me on GitHub"/>
+    </a>
+
+
+</div>
+<!-- /container -->
 
 <!-- modal(s) -->
-	<div class="modal hide fade" id="countdownModal" tabindex="-1" role="dialog" aria-labelledby="countdownLabel" aria-hidden="true">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h3 id="countdownLabel">&nbsp;</h3>
-  </div>
-  <div class="modal-body">
-  	<p><div style="text-align: center;"><h1 id="countdownTime">5:00</h1></div></p>
-    <p><div class="progress progress-striped active">
-  	<div class="bar" style="width: 100%;" id="countdownBar"></div>
-</div></p>
-  </div>
-  <div class="modal-footer">
-    <button class="btn btn-primary btn-large" data-dismiss="modal" aria-hidden="true" id="btn-reset"><i class="icon-repeat icon-white"></i>
-    	Reset</button>    
-  </div>
-  </div>
+<div class="modal hide fade" id="countdownModal" tabindex="-1" role="dialog" aria-labelledby="countdownLabel" aria-hidden="true">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3 id="countdownLabel">&nbsp;</h3>
+    </div>
+    <div class="modal-body">
+        <div style="text-align: center;"><h1 id="countdownTime">5:00</h1></div>
+        <div class="progress progress-striped active">
+            <div class="bar" style="width: 100%;" id="countdownBar"></div>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button class="btn btn-primary btn-large" data-dismiss="modal" aria-hidden="true" id="btn-reset"><i
+                class="icon-repeat icon-white"></i>
+            Reset
+        </button>
+    </div>
+</div>
 
-    <!-- scripts -->
-	
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-    
-    <script type="text/javascript" src="js/timer2.js"></script>
-    <script type="text/javascript" src="js/teas_cs.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap-button.js"></script>
-    <script type="text/javascript" src="js/bootstrap-tooltip.js"></script>
-    <script type="text/javascript" src="js/bootstrap-popover.js"></script>
+<!-- scripts -->
 
-  </body>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
+
+<script type="text/javascript" src="js/timer2.js"></script>
+<script type="text/javascript" src="js/teas_cs.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-button.js"></script>
+<script type="text/javascript" src="js/bootstrap-tooltip.js"></script>
+<script type="text/javascript" src="js/bootstrap-popover.js"></script>
+
+</body>
 </html>
